e9200ed5d4e15de4385cd0f5061d6b9cebb8449f
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-10-03 21:53:49 -0300

    Clase 2

--- js/main.js
+++ js/main.js
@@ -1,115 +1,223 @@
-// MI PRIMER COMENTARIO DE UNA LINEA
+const var1 = "Hola";
+const var2 = "Como estas";
 
-/*
-
-LINEA 1 DE COMENTARIO
-LINEA 2 DE COMENTARIO
-LINEA 3 COMENTARIO
-
-*/
-
-// Declarar variables
-// let nombre;
-// const apellido = "Perez";
-// let edad = 20;
-// const sexo = "Masculino";
-
-// Cambiando el valor de variables
-// edad = 21;
-// nombre = "Juan";
-
-// Puedo cambiar el tipo de valor de una variable
-// nombre = 10;
-
-// Operaciones con números
-// const numero1 = 15;
-// const numero2 = 30;
+// Ifs sencillos
+// if (true) {
+//     console.log(var1);
+// }
 //
-// const resultadoSuma = numero1 + numero2;
+// if (false) {
+//     console.log(var2);
+// }
+
+// Operador de comparación
+// if(var1 == "Hola") {
+//     console.log("La variable var1 es: Hola");
+// }
 //
-// const resultadoResta = numero2 - numero1;
+// if(var2 == "Como estas") {
+//     console.log("La variable var2 es: Como esta");
+// }
 //
-// const resultadoMultiplicacion = numero1 * numero2;
+// if(var1 == "hola") { // Esto es falso dado que JS es case senstivie
+//     console.log("la variable var1 es: Hola");
+// }
 //
-// const resultadoDivision = numero2 / numero1;
+// const numero1 = 5;
+// const numero2 = 20;
 //
-// const resultadoCombinado = (numero1 + numero2) / numero2 * (numero1 * numero2);
-
-// Operaciones con texto
-const string1 = "Hola";
-const string2 = "Que tal";
-const espacioEnBlanco = " ";
-
-const resultadoConcatenacion = string1 + string2; // Resultado: HolaQue tal
-
-const resultadoCombinadoConcatenacion = string1 + string2 + string1 + string2 + string2 + string1;
-
-const resultadoConEspacioForma1 = string1 + " " + string2; // Resultado forma 1: Hola Que tal
-const resultadoConEspacioForma2 = string1 + espacioEnBlanco + string2; // Resultado forma 2: Hola Que tal
-
-// Imprimir valores
-// console.log(resultadoSuma);
-// console.log(resultadoConcatenacion);
-
-// Pedirle valores al usuario
-// const nombre = prompt("Ingrese un nombre");
-// const edad = prompt("Ingrese la edad");
+// const resultado = numero1 + numero2;
 //
-// console.log("El nombre de la persona es: " + nombre);
-// console.log("La edad de " + nombre + " es: " + edad);
+// if(resultado == 15) {
+//     console.log("La suma da 15");
+// }
 
-// Mostrar valores en alert
-// alert("El nombre de la persona es: " + nombre);
-// alert("La edad de " + nombre + " es: " + edad);
-
-// Tipos de valores de variables
-// const numero1 = prompt("Ingrese un número");
-// const numero2 = prompt("Ingrese otro número");
+// Diferencia de = y ==
+// let variabledeprueba = 5;
 //
-// const resultadoSuma = parseInt(numero1) + parseInt(numero2);
+// if(variabledeprueba = 10) {
+//     console.log(variabledeprueba);
+//     console.log("la variable es 5");
+// }
 
-// console.log("El resultado de la suma es: " + resultadoSuma);
-
-// const var1 = 10;
-// const var2 = 30;
-// const var3 = 50;
-// const var4 = "60";
+// IF ELSE
+// Ejemplo 1
+// const numero1 = 3;
+// const numero2 = 6;
+// const resultado = numero1 * numero2;
 //
-// const resultadoVar = var1 + var2 + var3 + parseInt(var4);
+// if(resultado == 15) {
+//     console.log("El resultado es igual a 15");
+// } else {
+//     console.log("El resultado no es 15");
+// }
+
+// Ejemplo 2
+// const nombre = prompt("Ingrese su nombre");
 //
-// console.log(resultadoVar);
+// if(nombre == "Rodrigo") {
+//     console.log("Acceso permitido");
+// } else {
+//     console.log("Acceso denegado");
+// }
 
-// Valores enteros y decimales
-// const numero = "10.5";
+// IF - ELSE IF - ELSE
+// const nombre = prompt("Ingrese su nombre");
 //
-// console.log(parseInt(numero));
-// console.log(parseFloat(numero));
+// if(nombre == "Rodrigo") {
+//     console.log("Bienvenido Rodrigo");
+// } else if(nombre == "Juan") {
+//     console.log("Bienvenido Juan");
+// } else if(nombre == "Pedro") {
+//     console.log("Bienvenido Pedro");
+// } else {
+//     console.log("No ingresaste un nombre válido");
+// }
 
-// Ejemplo de lo que vimos hasta ahora
-// const numero1 = parseInt(prompt("Ingrese un número"));
-// const numero2 = parseInt(prompt("Ingrese un número"));
+// Variables booleanas
+// const verdadero = true;
+// const falso = false;
+
+// const numero1 = 10;
+// const esNumero10 = (numero1 == 10);
 //
-// const multiplicacion = numero1 * numero2;
-// const division = numero1 / numero2;
-// const resta = numero1 - numero2;
-// const suma = numero1 + numero2;
+// if (esNumero10) {
+//     console.log("Es numero 10");
+// } else {
+//     console.log("No es numero 10");
+// }
+
+// Evaluar números
+// const numero = -10;
 //
-// console.log("El resultado de la multiplicación es: " + multiplicacion);
-// console.log("El resultado de la divisón es: " + division);
-// console.log("el resultado de la resta es: " + resta);
-// console.log("El resultado de la suma es: " + suma);
+// if (numero) {
+//     console.log("asd");
+// }
 
-// Prompt con una variable let
-// let nombre = prompt("Ingrese un nombre");
-// nombre = nombre + " " + prompt("Ingres el apellido");
+// Operador "es distinto"
+// const numero1 = 20;
+// const numero2 = 20;
+
+// if(numero1 == numero2) {
+//     console.log("Numero 1 es igual a numero 2");
+// } else {
+//     console.log("numero 1 es distinto de numero 2");
+// }
+
+// if (numero1 != numero2) {
+//     console.log("numero 1 es distinto de numero 2");
+// } else {
+//     console.log("Numero 1 es igual a numero 2");
+// }
+
+// Operador lógicos de números
+
+// const precioProducto1 = parseInt(prompt("Ingrese el precio de producto 1"));
+// const precioProducto2 = parseInt(prompt("Ingrese el precio de producto 2"));
+
+// if (precioProducto1 >= precioProducto2) {
+//     console.log("El precio del producto 1 es mayor o igual al precio del producto 2");
+// } else {
+//     console.log("El precio del producto 1 es menor al precio del producto 2");
+// }
+
+// if (precioProducto1 <= precioProducto2) {
+//     console.log("El precio del producto 1 es menor al precio del producto 2");
+// } else {
+//     console.log("El precio del producto 1 es mayor o igual al precio del producto 2");
+// }
+
+// const edad = parseInt(prompt("Ingrese su edad"));
 //
-// console.log(nombre);
+// if(edad >= 18) {
+//     console.log("Puede comprar alcohol");
+// } else {
+//     console.log("No puede comprar, es menor de edad");
+// }
 
-// Ejemplo de pedir un número y sumarlo a otro que tengo
-const numero1 = 50;
-const numero2 = prompt("Ingrese el número");
+// Operador &&
 
-const resultadoNumero = numero1 + parseInt(numero2);
+// const nombre = "Pedro";
+// const edad = 25;
 
-console.log("El resultado de la suma es: " + resultadoNumero);
+// if (nombre == "Rodrigo") {
+//
+//     if(edad == 20) {
+//         console.log("El nombre es Rodrigo y la edad es 20");
+//     }
+// }
+
+// if ( (nombre == "Rodrigo") && (edad == 20) ) {
+//     console.log("El nombre es Rodrigo y la edad es 20");
+// } else if( (nombre != "Rodrigo")) {
+//     console.log("El nombre es distinto a Rodrigo");
+// } else if( (edad != 20)) {
+//     console.log("La edad es distinta de 20");
+// }
+
+// Operador ||
+
+// const nombre = prompt("Ingrese el nombre");
+//
+// if( (nombre == "Pedro") || (nombre == "pedro") || (nombre == "PEDRO") ) {
+//     console.log("BIenvenido pedro");
+// } else {
+//     console.log("Usted no es pedro");
+// }
+
+// Operadores && y || compuestos
+
+// const nombre = prompt("Ingrese el nombre");
+// const edad = parseInt(prompt("Ingrese la edad"));
+//
+// if ( (edad >= 18) && (nombre == "Pedro" || nombre == "pedro") ) {
+//     console.log("Permitir acceso");
+// } else {
+//     console.log("Acceso denegado");
+// }
+
+// CUIDADO CON LOS PARENTESIS
+
+// const nombre = prompt("Ingrese el nombre");
+// const edad = parseInt(prompt("Ingrese la edad"));
+//
+// if ( (edad >= 18) && (nombre == "Pedro" || nombre == "pedro") ) {
+//     console.log("Permitir acceso");
+// } else {
+//     console.log("Acceso denegado");
+// }
+
+/**
+ * EJERCICIO 7
+ *
+ * Escriba un programa en donde se le pida al usuario ingresar el lugar a donde quiera
+ * viajar. Teniendo en cuenta estos valores:
+ * - Colombia: $40
+ * - México: $50
+ * - Brasil: $30
+ * - Uruguay: $20
+ * - Chile: $20
+ * - Ecuador: $30
+ * - Perú: $40
+ * - Estados Unidos: $50
+ * Mostrarle al usuario cuál sería el costo de viajar al lugar que escribió.
+ *     NOTA: Tener en cuenta las mayúsculas y minúsculas al momento de ingresar el
+ * nombre del lugar
+ */
+
+let costo = 0;
+const lugarAViajar = prompt("Ingrese a dónde quiere viajar");
+
+if(lugarAViajar == "Colombia" || lugarAViajar == "Perú") {
+    costo = 40;
+} else if(lugarAViajar == "México" || lugarAViajar == "Estados Unidos") {
+    costo = 50;
+} else if(lugarAViajar == "Uruguay" || lugarAViajar == "Chile") {
+    costo = 20;
+} else if(lugarAViajar == "Ecuador" || lugarAViajar == "Brasil") {
+    costo = 30;
+}
+
+console.log("El costo de su viaje a " + lugarAViajar + " es: $" + costo);
+
 
