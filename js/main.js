04ee365e68246616cf72c11a6ae793119890bbe8
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-10-05 21:40:19 -0300

    + Clase 3

--- js/main.js
+++ js/main.js
@@ -1,223 +1,273 @@
-const var1 = "Hola";
-const var2 = "Como estas";
+// const nombre1 = prompt("Ingrese un nombre");
+// console.log(nombre1);
+//
+// const nombre2 = prompt("Ingrese un nombre");
+//
+// const nombre3 = prompt("Ingrese un nombre");
+//
+// const nombre4 = prompt("Ingrese un nombre");
 
-// Ifs sencillos
-// if (true) {
-//     console.log(var1);
+// ....
+
+/**
+ * FOR
+ */
+
+// for(let i = 10; i <= 20; i = i + 1) {
+//     console.log(i);
+// }
+
+// console.log("Finaliza el for");
+
+// Pedir 10 nombres
+// let nombre = "";
+//
+// for(let i = 0; i < 5; i = i + 1) {
+//
+//     nombre = prompt("Ingrese un nombre");
+//     console.log(nombre);
+//
+// }
+
+// Break
+// // Números pares
+// for(let i = 0; i <= 10; i = i + 2) {
+//
+//     if(i >= 5) {
+//         break;
+//     }
+//
+//     console.log(i);
+// }
+
+// // Números impares
+// for(let i = 1; i <= 10; i = i + 2) {
+//
+//     if(i > 5) {
+//         break;
+//     }
+//
+//     console.log(i);
+// }
+
+// Continue
+// Números pares
+// for(let i = 0; i <= 10; i++) {
+//
+//     if(i % 2 === 0) {
+//         continue;
+//     }
+//
+//     console.log(i);
+// }
+
+// Números pares
+// for(let i = 0; i <= 10; i++) {
+//
+//     if(i % 2 !== 0) {
+//         continue;
+//     }
+//
+//     console.log(i);
 // }
 //
-// if (false) {
-//     console.log(var2);
-// }
+// console.log("Finalizó el ciclo");
 
-// Operador de comparación
-// if(var1 == "Hola") {
-//     console.log("La variable var1 es: Hola");
-// }
+/**
+ * WHILE
+ */
+
+// // Pedir nombres
+// // Forma 1
+// let nombre = "";
 //
-// if(var2 == "Como estas") {
-//     console.log("La variable var2 es: Como esta");
-// }
+// while(nombre !== "SALIR") {
 //
-// if(var1 == "hola") { // Esto es falso dado que JS es case senstivie
-//     console.log("la variable var1 es: Hola");
-// }
+//     nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar más");
 //
-// const numero1 = 5;
-// const numero2 = 20;
-//
-// const resultado = numero1 + numero2;
-//
-// if(resultado == 15) {
-//     console.log("La suma da 15");
-// }
-
-// Diferencia de = y ==
-// let variabledeprueba = 5;
-//
-// if(variabledeprueba = 10) {
-//     console.log(variabledeprueba);
-//     console.log("la variable es 5");
-// }
-
-// IF ELSE
-// Ejemplo 1
-// const numero1 = 3;
-// const numero2 = 6;
-// const resultado = numero1 * numero2;
-//
-// if(resultado == 15) {
-//     console.log("El resultado es igual a 15");
-// } else {
-//     console.log("El resultado no es 15");
-// }
-
-// Ejemplo 2
-// const nombre = prompt("Ingrese su nombre");
-//
-// if(nombre == "Rodrigo") {
-//     console.log("Acceso permitido");
-// } else {
-//     console.log("Acceso denegado");
-// }
-
-// IF - ELSE IF - ELSE
-// const nombre = prompt("Ingrese su nombre");
-//
-// if(nombre == "Rodrigo") {
-//     console.log("Bienvenido Rodrigo");
-// } else if(nombre == "Juan") {
-//     console.log("Bienvenido Juan");
-// } else if(nombre == "Pedro") {
-//     console.log("Bienvenido Pedro");
-// } else {
-//     console.log("No ingresaste un nombre válido");
-// }
-
-// Variables booleanas
-// const verdadero = true;
-// const falso = false;
-
-// const numero1 = 10;
-// const esNumero10 = (numero1 == 10);
-//
-// if (esNumero10) {
-//     console.log("Es numero 10");
-// } else {
-//     console.log("No es numero 10");
-// }
-
-// Evaluar números
-// const numero = -10;
-//
-// if (numero) {
-//     console.log("asd");
-// }
-
-// Operador "es distinto"
-// const numero1 = 20;
-// const numero2 = 20;
-
-// if(numero1 == numero2) {
-//     console.log("Numero 1 es igual a numero 2");
-// } else {
-//     console.log("numero 1 es distinto de numero 2");
-// }
-
-// if (numero1 != numero2) {
-//     console.log("numero 1 es distinto de numero 2");
-// } else {
-//     console.log("Numero 1 es igual a numero 2");
-// }
-
-// Operador lógicos de números
-
-// const precioProducto1 = parseInt(prompt("Ingrese el precio de producto 1"));
-// const precioProducto2 = parseInt(prompt("Ingrese el precio de producto 2"));
-
-// if (precioProducto1 >= precioProducto2) {
-//     console.log("El precio del producto 1 es mayor o igual al precio del producto 2");
-// } else {
-//     console.log("El precio del producto 1 es menor al precio del producto 2");
-// }
-
-// if (precioProducto1 <= precioProducto2) {
-//     console.log("El precio del producto 1 es menor al precio del producto 2");
-// } else {
-//     console.log("El precio del producto 1 es mayor o igual al precio del producto 2");
-// }
-
-// const edad = parseInt(prompt("Ingrese su edad"));
-//
-// if(edad >= 18) {
-//     console.log("Puede comprar alcohol");
-// } else {
-//     console.log("No puede comprar, es menor de edad");
-// }
-
-// Operador &&
-
-// const nombre = "Pedro";
-// const edad = 25;
-
-// if (nombre == "Rodrigo") {
-//
-//     if(edad == 20) {
-//         console.log("El nombre es Rodrigo y la edad es 20");
+//     if(nombre !== "SALIR") {
+//         console.log("El nombre que ingresó es: " + nombre);
 //     }
 // }
-
-// if ( (nombre == "Rodrigo") && (edad == 20) ) {
-//     console.log("El nombre es Rodrigo y la edad es 20");
-// } else if( (nombre != "Rodrigo")) {
-//     console.log("El nombre es distinto a Rodrigo");
-// } else if( (edad != 20)) {
-//     console.log("La edad es distinta de 20");
+//
+// // Forma 2
+// let nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar más");
+//
+// while(nombre !== "SALIR") {
+//
+//     console.log("El nombre que ingresó es: " + nombre);
+//     nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar más");
+//
 // }
 
-// Operador ||
-
-// const nombre = prompt("Ingrese el nombre");
+// Contar de 1 a 10
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
 //
-// if( (nombre == "Pedro") || (nombre == "pedro") || (nombre == "PEDRO") ) {
-//     console.log("BIenvenido pedro");
-// } else {
-//     console.log("Usted no es pedro");
-// }
-
-// Operadores && y || compuestos
-
-// const nombre = prompt("Ingrese el nombre");
-// const edad = parseInt(prompt("Ingrese la edad"));
-//
-// if ( (edad >= 18) && (nombre == "Pedro" || nombre == "pedro") ) {
-//     console.log("Permitir acceso");
-// } else {
-//     console.log("Acceso denegado");
-// }
-
-// CUIDADO CON LOS PARENTESIS
-
-// const nombre = prompt("Ingrese el nombre");
-// const edad = parseInt(prompt("Ingrese la edad"));
-//
-// if ( (edad >= 18) && (nombre == "Pedro" || nombre == "pedro") ) {
-//     console.log("Permitir acceso");
-// } else {
-//     console.log("Acceso denegado");
+//     i = i + 1; // Forma 1
+//     // i++; // Forma 2
 // }
 
 /**
- * EJERCICIO 7
- *
- * Escriba un programa en donde se le pida al usuario ingresar el lugar a donde quiera
- * viajar. Teniendo en cuenta estos valores:
- * - Colombia: $40
- * - México: $50
- * - Brasil: $30
- * - Uruguay: $20
- * - Chile: $20
- * - Ecuador: $30
- * - Perú: $40
- * - Estados Unidos: $50
- * Mostrarle al usuario cuál sería el costo de viajar al lugar que escribió.
- *     NOTA: Tener en cuenta las mayúsculas y minúsculas al momento de ingresar el
- * nombre del lugar
+ * DO WHILE
  */
 
-let costo = 0;
-const lugarAViajar = prompt("Ingrese a dónde quiere viajar");
+// Diferencias usando while
+// let nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar nada.");
+//
+// while(nombre !== "SALIR") {
+//
+//     console.log(nombre);
+//     nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar nada.");
+// }
 
-if(lugarAViajar == "Colombia" || lugarAViajar == "Perú") {
-    costo = 40;
-} else if(lugarAViajar == "México" || lugarAViajar == "Estados Unidos") {
-    costo = 50;
-} else if(lugarAViajar == "Uruguay" || lugarAViajar == "Chile") {
-    costo = 20;
-} else if(lugarAViajar == "Ecuador" || lugarAViajar == "Brasil") {
-    costo = 30;
+// Usando do while
+// let nombre = "";
+//
+// do {
+//     nombre = prompt("Ingrese un nombre. Ingrese SALIR para no ingresar nada.");
+//
+//     if(nombre !== "SALIR") {
+//         console.log(nombre);
+//     }
+//
+// } while(nombre !== "SALIR");
+
+/**
+ * SWITCH
+ */
+
+// // Pedimos la operación al usuario
+// let operacion = prompt("Ingrese la operación que quiere realizar. 1- Transferir. 2- Depositar. 0- SALIR");
+//
+// // Mientras el usuario escriba algo distinto de SALIR, nos quedamos en el while
+// while(operacion !== "0") {
+//
+//     // Chequeamos que operación ingreso
+//     switch(operacion) {
+//
+//         // Transferir
+//         case "1":
+//
+//             let clienteATransferir = prompt("Ingrese el cliente al cual le quiere transferir. 1- Manuel. 2- Pepe. 3- Pedro. 0- SALIR");
+//
+//             // Mientras el cliente sea distinto de 0, estamos en el bucle
+//             while(clienteATransferir !== "0") {
+//
+//                 // Usuario ingrese monto a transferir
+//                 let monto = parseInt(prompt("Ingrese cantidad a transferir"));
+//
+//                 // Valido el monto ingresado
+//                 while(monto <= 0) {
+//                     monto = parseInt(prompt("Ingrese un monto válido a transferir"));
+//                 }
+//
+//                 /**
+//                  * CONFIRMAR OPERACIÓN
+//                  */
+//
+//                 // Forma 1
+//                 // const confirmar = prompt("Está seguro que desea transferir $" + monto + ". Ingrese SI para confirmar o cualquier cosa para abortar");
+//                 //
+//                 // if(confirmar !== "SI") {
+//                 //     break;
+//                 // }
+//
+//                 // Forma 2
+//                 const confirmar = prompt("Está seguro que desea transferir $" + monto + ". Ingrese NO para abortar, cualquier otra cosa para confirmar");
+//
+//                 if(confirmar === "NO") {
+//                     alert("TRANSFERENCIA ABORTADA");
+//                     break;
+//                 }
+//
+//                 /**
+//                  * FINALIZA CONFIRMAR OPERACIÓN
+//                  */
+//
+//                 // Chequeamos el usuario a transferir
+//                 switch(clienteATransferir) {
+//                     case "1": // Manuel
+//
+//                         const saldoDeManuel = 1000 + monto;
+//
+//                         console.log("Le transferiste $" + monto + " a Manuel. El nuevo saldo que tiene Manuel es $" + saldoDeManuel);
+//
+//                         break;
+//
+//                     case "2": // Pepe
+//
+//                         const saldoDePepe = 2000 + monto;
+//
+//                         console.log("Le transferiste $" + monto + " a Pepe. El nuevo saldo que tiene Pepe es $" + saldoDePepe);
+//
+//                         break;
+//
+//                     case "3": // Pedro
+//
+//                         const saldoDePedro = 3000 + monto;
+//
+//                         console.log("Le transferiste $" + monto + " a Pedro. El nuevo saldo que tiene Pedro es $" + saldoDePedro);
+//
+//                         break;
+//
+//                     default:
+//
+//                         alert("EL CLIENTE NO EXISTE");
+//
+//                         break;
+//                 }
+//
+//                 // Volvemos a pedir un usuario
+//                 clienteATransferir = prompt("Ingrese un nuevo cliente al cual le quiere transferir. 1- Manuel. 2- Pepe. 3- Pedro. 0- SALIR");
+//             }
+//
+//         break;
+//
+//         case "2":
+//
+//             console.log("Depositar");
+//             // ..
+//             // ..
+//
+//             break;
+//
+//         default:
+//
+//             alert("INGRESASTE UNA OPCIÓN INCORRECTA");
+//
+//             break;
+//     }
+//
+//     // Volver a pedirle una opción al usuario
+//     operacion = prompt("Ingrese la operación que quiere realizar. 1- Transferir. 2- Depositar. 0- SALIR");
+// }
+//
+// // Finaliza la aplicación
+// console.log("Gracias por utilizar nuestros servicios");
+
+/**
+ * EJERCICIO 10
+ *
+ * Escriba un programa en dónde se le solicite un número al usuario y determine si es
+ * un número primo o no.
+ */
+
+let divisores = 0;
+const numero = parseInt(prompt("Ingrese un número"));
+
+for(let i = numero; i >= 1; i = i - 1) {
+
+    if(numero % i === 0) {
+        divisores = divisores + 1;
+    }
 }
 
-console.log("El costo de su viaje a " + lugarAViajar + " es: $" + costo);
+if(divisores > 2) {
+    console.log("El número ingresado NO es primo");
+} else {
+    console.log("El número ingresado es primo");
+}
 
 
