ead09e39d6962e281bf0ecf873eaf575cd47fd4d
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-10-13 20:11:41 -0300

    After + objetos

--- js/main.js
+++ js/main.js
@@ -1,298 +1,142 @@
 // Funciones
-// function pedirNombre() {
-//
-//     const nombre = prompt("Ingrese el nombre");
-//     console.log("Su nombre es: " + nombre);
-//
-// }
+function concatenarCodigo(codigoDeProducto) {
 
-// function mostrarNombre(nombre) {
-//     console.log("Su nombre es: " + nombre);
-// }
-//
-// function sumar(numero1, numero2) {
-//
-//     const resultado = numero1 + numero2;
-//     console.log("La suma es: " + resultado);
-// }
-
-// Inicio
-// console.log("Inicio");
-// pedirNombre();
-// console.log("Fin");
-
-// mostrarNombre("Sergio");
-// mostrarNombre("Pedro");
-// sumar(10, 15);
-// sumar(1, 2);
-// sumar(40, 20);
-// sumar(40, 20);
-
-// Parámetros y valores de retorno
-
-// function sumar(numero1, numero2) {
-//
-//     // Forma 1
-//     // const resultado = numero1 + numero2;
-//     // return resultado;
-//
-//     // Forma 2
-//     return numero1 + numero2;
-// }
-//
-// const resultado = sumar(10, 30);
-//
-// console.log("La suma es: " + resultado);
-
-// function obtenerNombre() {
-//     return prompt("Ingrese el nombre");
-// }
-//
-// const nombre = obtenerNombre();
-//
-// console.log("El nombre de la persona es: " + nombre);
-
-// function concatenarNombreYApellido(nombre, apellido) {
-//
-//     // Devolves la concatenación
-//     return nombre + " " + apellido;
-//
-//     // O mostras la concatenación
-//     // console.log(nombre + " " + apellido);
-//
-// }
-
-// Obtener valor de la función
-// const nombreCompleto = concatenarNombreYApellido("Pedro", "Rodriguez");
-// console.log(nombreCompleto);
-
-// Mostrar la concatenación en la función
-// concatenarNombreYApellido("Pedro", "Rodriguez");
-
-// Scope
-
-// Variables globales
-// let numero = 10;
-//
-// if(true) {
-//
-//     numero = 5;
-//
-//     for(let i = 0; i < 10; i++) {
-//         numero += i;
-//     }
-//
-// }
-//
-// console.log(numero);
-
-// Variables locales
-
-// if(true) {
-//
-//     let numero = 5;
-//
-//     numero += 10;
-//
-//     for(let i = 0; i < 10; i++) {
-//         numero += i;
-//     }
-//
-//     console.log(numero);
-//
-// }
-//
-
-// Variables locales y globales
-
-// function suma(numero1, numero2) {
-//     let resultado = numero1 + numero2;
-//
-//     console.log(resultado);
-// }
-//
-// function resta(numero1, numero2) {
-//     let resultado = numero1 - numero2;
-//
-//     console.log(resultado);
-// }
-//
-// let resultado = 10;
-//
-// suma(5, 10);
-// resta(10, 5);
-//
-//
-// resultado += 20;
-// console.log(resultado);
-
-// Funciones anónimas
-
-// Ejemplo 1
-// const mostrarNombre = function (nombre) {
-//     console.log("El nombre es: " + nombre);
-// }
-
-// Ejemplo 2
-// function suma(numero1, numero2, accion) {
-//     const resultado = numero1 + numero2;
-//
-//     accion(resultado);
-// }
-//
-// const mostrarNumero = function (numero) {
-//     console.log("El numero es: " + numero);
-// }
-//
-// const sumarDiez = function (numero) {
-//     const resultado = numero + 10;
-//     console.log("Numero: " + resultado);
-// }
-//
-// suma(10, 15, mostrarNumero);
-// suma(30, 15, sumarDiez);
-
-// Función flecha
-
-// const mostrarNombre = function (nombre) {
-//     console.log("El nombre es: " + nombre);
-// }
-// const mostrarNombre = (nombre) => {
-//     console.log("El nombre es: " + nombre);
-// }
-
-// const devolverNombre = (nombre) => nombre;
-//
-// console.log(devolverNombre("Pablo"));
-
-/**
- * EJEMPLO DE PROYECTO FINAL
- */
-
-// Funciones
-function solicitarMonto() {
-    let monto = parseInt(prompt("Ingrese cantidad a transferir"));
-
-    // Valido el monto ingresado
-    while(monto <= 0) {
-        monto = parseInt(prompt("Ingrese un monto válido a transferir"));
+    if(codigosDeProducto !== "") {
+        codigosDeProducto += ", ";
     }
 
-    return monto;
+    codigosDeProducto += codigoDeProducto;
 }
 
-function transferir() {
-    let clienteATransferir = prompt("Ingrese el cliente al cual le quiere transferir. 1- Manuel. 2- Pepe. 3- Pedro. 0- SALIR");
+function agregarUnNuevoProducto () {
 
-    // Mientras el cliente sea distinto de 0, estamos en el bucle
-    while(clienteATransferir !== "0") {
+    let codigoDeProducto = pedirCodigo();
 
-        // Usuario ingrese monto a transferir
-        let monto = solicitarMonto();
+    while(codigoDeProducto !== "0") {
 
-        /**
-         * CONFIRMAR OPERACIÓN
-         */
+        switch(codigoDeProducto) {
 
-            // Forma 1
-            // const confirmar = prompt("Está seguro que desea transferir $" + monto + ". Ingrese SI para confirmar o cualquier cosa para abortar");
-            //
-            // if(confirmar !== "SI") {
-            //     break;
-            // }
+            case "0001":
+                concatenarCodigo(codigoDeProducto);
+                total += 10;
+                break;
 
-            // Forma 2
-        const confirmar = prompt("Está seguro que desea transferir $" + monto + ". Ingrese NO para abortar, cualquier otra cosa para confirmar");
+            case "0002":
+                concatenarCodigo(codigoDeProducto);
+                total += 20;
+                break;
 
-        if(confirmar === "NO") {
-            alert("TRANSFERENCIA ABORTADA");
+            case "0003":
+                concatenarCodigo(codigoDeProducto);
+                total += 30;
+                break;
+
+            case "0004":
+                concatenarCodigo(codigoDeProducto);
+                total += 40;
+                break;
+
+            default:
+                alert("CÓDIGO INCORRECTO");
+                break;
+        }
+
+        // Volvemos a solicitar el código
+        codigoDeProducto = pedirCodigo();
+    }
+
+}
+
+function mostrarElTotal () {
+    alert("Los productos agregados son: " + codigosDeProducto + ". El total de los productos es: $" + total);
+}
+
+function restarValorDelTotal (monto) {
+
+    if(monto > total) {
+        alert("El monto es mayor al total. Ingrese un monto menor");
+    } else {
+
+        total = total - monto;
+
+    }
+
+}
+
+function sacarProducto() {
+    let codigoDeProducto = pedirCodigo();
+
+    while(codigoDeProducto !== "0") {
+
+        switch(codigoDeProducto) {
+
+            case "0001":
+                restarValorDelTotal(10);
+                break;
+
+            case "0002":
+                restarValorDelTotal(20);
+                break;
+
+            case "0003":
+                restarValorDelTotal(30);
+                break;
+
+            case "0004":
+                restarValorDelTotal(40);
+                break;
+
+            default:
+                alert("CÓDIGO INCORRECTO");
+                break;
+        }
+
+        // Volvemos a solicitar el código
+        codigoDeProducto = pedirCodigo();
+    }
+}
+
+function pedirOperacion() {
+    return prompt("Que operación desea realizar? 1- Ingresar un nuevo producto / 2- Quiere ver el total de los productos ingresados / 3- Sacar producto / 0- Quiere SALIR");
+}
+
+function pedirCodigo() {
+    return prompt("Ingrese del producto / 0001 - $10 / 0002 - $20 / 0003 - $30 / 0004 - $40 / 0 - SALIR");
+}
+
+// Inicio del programa
+let codigosDeProducto = "";
+let total = 0;
+let operacion = pedirOperacion();
+
+while(operacion !== "0") {
+
+    // Chequeamos la operación que ingresó el usuario
+    switch(operacion) {
+
+        case "1":
+
+            agregarUnNuevoProducto();
             break;
-        }
 
-        /**
-         * FINALIZA CONFIRMAR OPERACIÓN
-         */
+        case "2":
 
-        // Chequeamos el usuario a transferir
-        switch(clienteATransferir) {
-            case "1": // Manuel
+            mostrarElTotal();
+            break;
 
-                const saldoDeManuel = 1000 + monto;
+        case "3":
+            sacarProducto();
+            break;
 
-                console.log("Le transferiste $" + monto + " a Manuel. El nuevo saldo que tiene Manuel es $" + saldoDeManuel);
+        default:
 
-                break;
-
-            case "2": // Pepe
-
-                const saldoDePepe = 2000 + monto;
-
-                console.log("Le transferiste $" + monto + " a Pepe. El nuevo saldo que tiene Pepe es $" + saldoDePepe);
-
-                break;
-
-            case "3": // Pedro
-
-                const saldoDePedro = 3000 + monto;
-
-                console.log("Le transferiste $" + monto + " a Pedro. El nuevo saldo que tiene Pedro es $" + saldoDePedro);
-
-                break;
-
-            default:
-
-                alert("EL CLIENTE NO EXISTE");
-
-                break;
-        }
-
-        // Volvemos a pedir un usuario
-        clienteATransferir = prompt("Ingrese un nuevo cliente al cual le quiere transferir. 1- Manuel. 2- Pepe. 3- Pedro. 0- SALIR");
+            alert("OPCIÓN INCORRECTA");
+            break;
     }
+
+    // Volvemos a solicitar la operación
+    operacion = pedirOperacion();
+
 }
 
-function deposito() {
-    console.log("Depositar");
-    // ..
-    // ..
-}
-
-function procesarOperacion(operacion) {
-
-    // Mientras el usuario escriba algo distinto de SALIR, nos quedamos en el while
-    while(operacion !== "0") {
-
-        // Chequeamos que operación ingreso
-        switch(operacion) {
-
-            // Transferir
-            case "1":
-
-                transferir();
-                break;
-
-            case "2":
-
-                deposito();
-                break;
-
-            default:
-
-                alert("INGRESASTE UNA OPCIÓN INCORRECTA");
-                break;
-        }
-
-        // Volver a pedirle una opción al usuario
-        operacion = prompt("Ingrese la operación que quiere realizar. 1- Transferir. 2- Depositar. 0- SALIR");
-    }
-}
-
-// Inicio
-
-// Pedimos la operación al usuario
-let operacion = prompt("Ingrese la operación que quiere realizar. 1- Transferir. 2- Depositar. 0- SALIR");
-
-procesarOperacion(operacion);
-
-// Finaliza la aplicación
-console.log("Gracias por utilizar nuestros servicios");
+alert("GRACIAS POR UTILIZAR NUESTROS SERVICIOS");
---
+++ js/objetos.js
@@ -0,0 +1,91 @@
+// const nombreDeProducto = "Arroz";
+// const precioDelProducto = 20;
+// const stockDelProducto = 10;
+//
+// const producto = {
+//     nombre: "Arroz",
+//     categoria: "Legumbres",
+//     precio: 20,
+//     stock: 10,
+// }
+//
+// const producto2 = {
+//     nombre: "Leche",
+//     categoria: "Lacteos",
+//     precio: 30,
+//     stock: 20,
+// }
+//
+// const producto3 = {
+//     nombre: "Yogurt",
+//     categoria: "Lacteos",
+//     precio: 60,
+//     stock: 5,
+// }
+
+// Función constructora
+// function Producto(nombre, categoria, precio, stock) {
+//     this.nombre = nombre;
+//     this.categoria = categoria;
+//     this.precio = precio;
+//     this.stock = stock;
+//     this.promocion = true;
+//
+//     this.mostrarPrecio = () => {
+//         console.log(this.precio);
+//     }
+// }
+//
+// const producto1 = new Producto("Arroz", "Legumbres", 20, 10);
+// const producto2 = new Producto("Leche", "Lacteos", 30, 20);
+// const producto3 = new Producto("Yogurt", "Lacteos", 60, 5);
+
+// producto1.mostrarPrecio();
+// producto1.precio = 150;
+// producto1.mostrarPrecio();
+
+// console.log(producto1);
+// console.log(producto2);
+// console.log(producto3);
+//
+// const producto4 = new Producto("Atún");
+//
+// console.log(producto4);
+
+// Clases
+// class Producto {
+//
+//     constructor(nombre, categoria, precio, stock) {
+//         this.nombre = nombre;
+//         this.categoria = categoria;
+//         this.precio = precio;
+//         this.stock = stock;
+//         this.promocion = true;
+//     }
+//
+//     mostrarPrecio() {
+//         console.log(this.precio);
+//     }
+// }
+//
+// const producto1 = new Producto("Arroz", "Legumbres", 20, 10);
+// const producto2 = new Producto("Leche", "Lacteos", 30, 20);
+// const producto3 = new Producto("Yogurt", "Lacteos", 60, 5);
+
+// Objetos parte 2
+
+// const nombre = "Rodrigo";
+//
+// console.log(nombre.includes("qweqwe"));
+
+// let numerito = 10;
+//
+// console.log(numerito.toString());
+
+// let var1 = "1";
+//
+// console.log(var1.includes("123"));
+//
+// var1 = 10;
+//
+// console.log(var1.toFixed(1));
