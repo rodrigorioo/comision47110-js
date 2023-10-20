a90b814f15e25443dc30c36fdb73b385e35faf06
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-10-20 20:18:28 -0300

    After

--- js/main.js
+++ js/main.js
@@ -1,379 +1,204 @@
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Leche",
-//     "Yogurt",
-//     "Fideos",
-//     "Zanahoria",
-// ];
-// const preciosDeProductos = [100, 200, 300, 400, 500, 600];
-
-// Acceder a los elementos individualmente
-// console.log(nombresDeProductos[0]);
-// console.log(preciosDeProductos[2]);
-
-// Recorrer los nombres de los productos
-// for (let i = 0; i < 4; i++) {
-//     console.log(nombresDeProductos[i]);
+// // Función flecha
+//
+// // function suma(numero1, numero2) {
+// //     return numero1 + numero2;
+// // }
+//
+// const suma = function(numero1, numero2) {
+//     return numero1 + numero2;
 // }
-
-// for (let i = 0; i < 6; i++) {
-//     console.log(preciosDeProductos[i]);
-// }
-
-// Propiedad LENGTH
-// for (let i = 0; i < nombresDeProductos.length; i++) {
-//     console.log(nombresDeProductos[i]);
-// }
-
-// Agregar elementos al array
-
-// Agregar al final
-// nombresDeProductos.push("Azucar");
-// nombresDeProductos.push("Cereales");
-// Agregar al principio
-// nombresDeProductos.unshift("Papa");
-
-// for (let i = 0; i < nombresDeProductos.length; i++) {
-//     console.log(nombresDeProductos[i]);
-// }
-
-// Quitar los elementos
-// console.log(nombresDeProductos);
 //
-// nombresDeProductos.shift();
-// nombresDeProductos.pop();
+// // const suma = (numero1, numero2) => numero1 + numero2;
 //
-// console.log(nombresDeProductos);
-
-// Eliminar varios elementos
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Zanahoria",
-//     "Papa",
-//     "Huevo",
-//     "Yogurt",
-// ];
-
-// nombresDeProductos.splice(2, 2);
-
-// Mostrar elementos como string
-// const listaDeProductos = nombresDeProductos.join(" - ");
-//
-// console.log(listaDeProductos);
-
-// Concatenar arrays
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Zanahoria",
-//     "Papa",
-//     "Huevo",
-//     "Yogurt",
-// ];
-// const otrosNombresDeProductos = [
-//     "Cereales",
-//     "Azucar",
-// ];
-//
-// const combinacionDeNombresDeProductos = nombresDeProductos.concat(otrosNombresDeProductos);
-//
-// console.log(nombresDeProductos);
-// console.log(otrosNombresDeProductos);
-// console.log(combinacionDeNombresDeProductos);
-
-// Obtener copia de array
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Zanahoria",
-//     "Papa",
-//     "Huevo",
-//     "Yogurt",
-// ];
-//
-// const copiaDeProductos = nombresDeProductos.slice(1, 4);
-//
-// console.log(copiaDeProductos);
-
-// Obtener índice dónde se encuentra el elemento
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Zanahoria",
-//     "Papa",
-//     "Huevo",
-//     "Yogurt",
-// ];
-//
-// const indiceDeHuevo = nombresDeProductos.indexOf("Arroz");
-//
-// console.log(indiceDeHuevo);
-
-// Chequear si existe el elemento
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Zanahoria",
-//     "Papa",
-//     "Huevo",
-//     "Yogurt",
-// ];
-//
-// if (nombresDeProductos.includes("Zanahoria")) {
-//     console.log("Se encuentra zanahoria");
-// }
-
-// Reverse
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Albahaca",
-//     "Huevo",
-//     "Papa",
-//     "Yogurt",
-//     "Zanahoria",
-// ];
-
-// console.log(nombresDeProductos);
-
-// nombresDeProductos.reverse();
-//
-// console.log(nombresDeProductos);
-
-// For of
-// const nombresDeProductos = [
-//     "Arroz",
-//     "Albahaca",
-//     "Huevo",
-//     "Papa",
-//     "Yogurt",
-//     "Zanahoria",
-// ];
-//
-// for (const nombreDeProducto of nombresDeProductos) {
-//     console.log(nombreDeProducto);
-// }
+// const resultado = suma(10, 20);
 
 /**
- * EJEMPLO 1 SIN CHEQUEAR STOCK
+ * 3) Dados los siguientes arrays
+ * a) [1, 3, 5, 6, 7, 9]
+ * b) [1, 2, 3, 4, 7, 8]
+ * Generar un nuevo array con la intersección de los elementos
  */
 
-// // Objetos
-// class Producto {
+// const interseccion = [];
+// const array1 = [1, 3, 5, 6, 7, 9];
+// const array2 = [1, 2, 3, 4, 7, 8];
+// // La intersección de ambos sería:
+// // [1, 3, 7]
 //
-//     constructor (nombre, precio, stock) {
-//         this.nombre = nombre;
-//         this.precio = precio;
-//         this.stock = stock;
+// for(const numero of array1) {
+//
+//     if(array2.includes(numero)) {
+//         interseccion.push(numero);
 //     }
 //
 // }
 //
-// // Funciones
-// function existeElProducto(nombreDeProducto) {
-//
-//     let encontrado = false;
-//
-//     for(const producto of listaDeProductos) {
-//
-//         // Si la propiedad nombre del objeto, es igual al nombre que paso por parámetro
-//         if(producto.nombre === nombreDeProducto) {
-//             encontrado = true;
-//             break;
-//         }
-//
-//     }
-//
-//     return encontrado;
-// }
-//
-// function agregarUnProducto() {
-//
-//     // Le pido el nombre del producto al usuario
-//     let productoAIngresar = prompt("Ingrese el nombre del producto que quiere agregar.");
-//
-//     // Mientras no exista un producto con el nombre que ingresó el usuario, se lo vuelvo a pedir
-//     while (!existeElProducto(productoAIngresar)) {
-//         productoAIngresar = prompt("Ingrese un nombre válido de un producto");
-//     }
-//
-//     // Pedirle el stock
-//     let stock = parseInt(prompt("Ingrese el stock que quiere agregar."));
-//
-//     while (stock <= 0) {
-//         stock = parseInt(prompt("Ingrese un stock válido (Mayor a 0)"));
-//     }
-//
-//     // Una vez que tenemos el nombre y el stock del producto a agregar, lo cargamos al carrito
-//     carrito.push({
-//         nombre: productoAIngresar,
-//         cantidad: stock,
-//     });
-//
-//     alert("PRODUCTO AGREGADO CON ÉXITO :D");
-//
-// }
-//
-// function finalizarCompra() {
-//
-//     let mensaje = "Los productos que agregó son: \n";
-//
-//     for(const productoAgregadoAlCarrito of carrito) {
-//
-//         mensaje += productoAgregadoAlCarrito.nombre + " - Cantidad: " + productoAgregadoAlCarrito.cantidad + "\n";
-//
-//     }
-//
-//     alert(mensaje);
-//
-// }
-//
-// // Inicio del programa
-// const listaDeProductos = [
-//     new Producto("Arroz", 50, 5),
-//     new Producto("Albahaca", 25, 10),
-//     new Producto("Huevo", 35, 2),
-//     new Producto("Papa", 20, 30),
-//     new Producto("Yogurt", 75, 2),
-// ];
-// const carrito = [];
-//
-// let operacion = prompt("Ingrese la operación que quiere realizar: 1- Agregar un producto / 2- Finalizar la compra / 0- Salir");
-//
-// while (operacion !== "0") {
-//
-//     // Chequeamos que operación ingresó
-//     switch(operacion) {
-//
-//         case "1":
-//
-//             agregarUnProducto();
-//
-//             break;
-//
-//         case "2":
-//
-//             finalizarCompra();
-//
-//             break;
-//
-//         default:
-//
-//             alert("OPCIÓN INCORRECTA");
-//             break;
-//     }
-//
-//     // Volvemos a solicitar la operación
-//     operacion = prompt("Ingrese la operación que quiere realizar: 1- Agregar un producto / 2- Finalizar la compra / 0- Salir");
-// }
+// console.log(interseccion);
 
 /**
- * EJEMPLO 2 COMPLETO CHEQUEANDO STOCK
+ * 4) Ídem ejercicio anterior pero en vez de la intersección, generar un nuevo array con la
+ * unión de los elementos (Sin repetirlos).
  */
+
+// const union = [];
+// const array1 = [1, 3, 5, 6, 7, 9];
+// const array2 = [1, 2, 3, 4, 7, 8];
+// // La unión sería:
+// // [1, 3, 5, 6, 7, 9, 2, 4, 8]
+//
+// // FORMA 1 - CON CONCAT
+// const concatenacion = array1.concat(array2);
+//
+// for(const elemento of concatenacion) {
+//
+//     if(!union.includes(elemento)) {
+//         union.push(elemento);
+//     }
+// }
+//
+// console.log(union);
+//
+// // HACERLO DE OTRA FORMA PARA CASA (?
+
+/** **************** */
+/** **************** */
+/**
+ * FUNCIONES DE ORDEN SUPERIOR
+ */
+/** **************** */
+/** **************** */
+// Funciones
+// function calcularEnvioLegumbres(precio) {
+//     return 100 + (precio * 0.05);
+// }
+//
+// function calcularEnvioLacteos(precio) {
+//
+//     let costo = 50;
+//
+//     if(precio > 100) {
+//         costo += 10;
+//     } else {
+//         costo += 5;
+//     }
+//
+//     return costo;
+// }
+//
+// function calcularEnvioVerduras(precio) {
+//     return (precio * 0.30);
+// }
+
+// FORMA MALA >:(
+// function calcularEnvio(producto) {
+//
+//     let costo = 0;
+//
+//     switch(producto.categoria) {
+//         case "Legumbres":
+//             costo = calcularEnvioLegumbres(producto.precio);
+//             break;
+//
+//         case "Lacteos":
+//             costo = calcularEnvioLacteos(producto.precio);
+//             break;
+//
+//         case "Verduras":
+//             costo = calcularEnvioVerduras(producto.precio);
+//             break;
+//     }
+//
+//     return costo;
+// }
+
+// FORMA BUENA :D
+// function calcularEnvio(producto, funcionQueCalculaElEnvio) {
+//
+//     let costo = funcionQueCalculaElEnvio(producto.precio);
+//
+//     return costo;
+// }
 
 // Objetos
-class Producto {
+// class Producto {
+//
+//     constructor(nombre, categoria, precio) {
+//         this.nombre = nombre;
+//         this.categoria = categoria;
+//         this.precio = precio;
+//     }
+// }
+//
+// const producto1 = new Producto("Lentejas", "Legumbres", 200);
+// const producto2 = new Producto("Yogurt", "Lacteos", 300);
+// const producto3 = new Producto("Zanahoria", "Verduras", 100);
 
-    constructor (nombre, precio, stock) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
+// FORMA MALA >:(
+// const costoLentejas = calcularEnvio(producto1);
+// const costoYogurt = calcularEnvio(producto2);
+// const costoZanahoria = calcularEnvio(producto3);
+
+// FORMA BUENA :D
+// const costoLentejas = calcularEnvio(producto1, calcularEnvioLegumbres);
+// const costoYogurt = calcularEnvio(producto2, calcularEnvioLacteos);
+// const costoZanahoria = calcularEnvio(producto1, calcularEnvioVerduras);
+
+// console.log(costoLentejas);
+// console.log(costoYogurt);
+
+
+// const numeros = [1, 2, 3, 4];
+// const nombres = ["Rodrigo", "Federico", "Gonzalo"];
+
+// forEach
+// numeros.forEach( (el) => {
+//     console.log(el * 2);
+// });
+// nombres.forEach( (qwkmesjkfasnkjfdajnks) => {
+//
+//     console.log("Hola " + qwkmesjkfasnkjfdajnks);
+// });
+
+// Reduce
+const carrito = [
+    {
+        nombre: "Pepino",
+        precio: 200,
+    },
+    {
+        nombre: "Yogurt",
+        precio: 100,
+    },
+    {
+        nombre: "Lechuga",
+        precio: 300,
     }
-
-}
-
-// Funciones
-
-function obtenerProducto(nombreDeProducto) {
-
-    let encontrado = false;
-
-    for(const producto of listaDeProductos) {
-
-        // Si la propiedad nombre del objeto, es igual al nombre que paso por parámetro
-        if(producto.nombre === nombreDeProducto) {
-            encontrado = producto;
-            break;
-        }
-
-    }
-
-    return encontrado;
-}
-
-function agregarUnProducto() {
-
-    // Le pido el nombre del producto al usuario
-    let productoAIngresar = prompt("Ingrese el nombre del producto que quiere agregar.");
-
-    // Mientras no obtenga un producto con el nombre que ingresó el usuario, se lo vuelvo a pedir
-    let producto = obtenerProducto(productoAIngresar);
-    while (!producto) {
-        productoAIngresar = prompt("Ingrese un nombre válido de un producto");
-        producto = obtenerProducto(productoAIngresar);
-    }
-
-    // Pedirle el stock
-    let stock = parseInt(prompt("Ingrese el stock que quiere agregar."));
-
-    while (stock <= 0 || stock > producto.stock) {
-        stock = parseInt(prompt("Ingrese un stock válido (Mayor a 0)"));
-    }
-
-    // Una vez que tenemos el nombre y el stock del producto a agregar, lo cargamos al carrito
-    carrito.push({
-        nombre: productoAIngresar,
-        cantidad: stock,
-    });
-
-    alert("PRODUCTO AGREGADO CON ÉXITO :D");
-
-}
-
-function finalizarCompra() {
-
-    let mensaje = "Los productos que agregó son: \n";
-
-    for(const productoAgregadoAlCarrito of carrito) {
-
-        mensaje += productoAgregadoAlCarrito.nombre + " - Cantidad: " + productoAgregadoAlCarrito.cantidad + "\n";
-
-    }
-
-    alert(mensaje);
-
-}
-
-// Inicio del programa
-const listaDeProductos = [
-    new Producto("Arroz", 50, 5),
-    new Producto("Albahaca", 25, 10),
-    new Producto("Huevo", 35, 2),
-    new Producto("Papa", 20, 30),
-    new Producto("Yogurt", 75, 2),
 ];
-const carrito = [];
 
-let operacion = prompt("Ingrese la operación que quiere realizar: 1- Agregar un producto / 2- Finalizar la compra / 0- Salir");
+// Forma sin reduce
+// let total = 0;
+// carrito.forEach( (producto) => {
+//
+//     total += producto.precio;
+//
+// });
 
-while (operacion !== "0") {
+// Forma con reduce
+// const total = carrito.reduce( (acc, producto) => {
+//
+//     acc += producto.precio;
+//
+//     return acc;
+// }, 0);
 
-    // Chequeamos que operación ingresó
-    switch(operacion) {
+// Agregar campo con reduce
+const carritoConCostoDeEnvio = carrito.reduce( (acc, producto) => {
 
-        case "1":
+    producto.costoDeEnvio = (producto.precio * 0.35);
 
-            agregarUnProducto();
+    acc.push(producto);
 
-            break;
+    return acc;
+}, []);
 
-        case "2":
-
-            finalizarCompra();
-
-            break;
-
-        default:
-
-            alert("OPCIÓN INCORRECTA");
-            break;
-    }
-
-    // Volvemos a solicitar la operación
-    operacion = prompt("Ingrese la operación que quiere realizar: 1- Agregar un producto / 2- Finalizar la compra / 0- Salir");
-}
+console.log(carritoConCostoDeEnvio);
